dfd7457cec4e6c8ba27ffc9b3d4a4d09a61127c1
Sergey Yuzefovich <questfo@gmail.com>
2016-04-21 11:35:08 +0200

    Better History version 3.9.10 - new button added - Back to Today

--- scripts/views/timeline_view.js
+++ scripts/views/timeline_view.js
@@ -1,11 +1,13 @@
 (function() {
+	var todayVisible = false;
   var TimelineView = BH.Views.MainView.extend({
     template: 'timeline.html',
 
     events: {
       'click a.date': 'onDateClicked',
       'click a.next': 'onNextClicked',
-      'click a.previous': 'onPreviousClicked'
+      'click a.previous': 'onPreviousClicked',
+      'click ol.today': 'onTodayClicked'      
     },
 
     initialize: function() {
@@ -17,12 +19,17 @@
     },
 
     render: function() {
+    	if ( $('.today').is(":visible")  )
+    		todayVisible = true; 
+    		    	
       this.$el.html('');
 
       var timelinePresenter = new BH.Presenters.Timeline(this.model.toJSON());
       var template = BH.Lib.Template.fetch(this.template);
       var properties = timelinePresenter.timeline(this.state.get('startDate'));
       this.$el.append(Mustache.to_html(template, properties));
+
+      todayVisible && $('.today').show(); 
 
       return this;
     },
@@ -36,6 +43,15 @@
       $(ev.currentTarget).addClass('selected');
     },
 
+    onTodayClicked: function(ev) {
+      if(!$(ev.currentTarget).hasClass('disabled')) {
+        var date = moment();
+        this.state.set({startDate: date.startOf('day').toDate()});
+      }
+      $('.today').hide(); 
+      todayVisible = false;
+    }, 
+
     onNextClicked: function(ev) {
       ev.preventDefault();
 
@@ -43,6 +59,7 @@
         var date = moment(this.state.get('startDate')).add('days', 7);
         this.state.set({startDate: date.startOf('day').toDate()});
       }
+      $('.today').show();      
     },
 
     onPreviousClicked: function(ev) {
@@ -52,6 +69,7 @@
         var date = moment(this.state.get('startDate')).subtract('days', 7);
         this.state.set({startDate: date.startOf('day').toDate()});
       }
+      $('.today').show();      
     }
   });
 
